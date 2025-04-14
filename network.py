1a0611c677e8c92f0bca0a0b94268acaace9f020
micaelsimoes <micaelsimoes@gmail.com>
2025-04-14 10:00:24 +0100

    Initial commit

---
+++ network.py
@@ -0,0 +1,24 @@
+# ======================================================================================================================
+#   Class NETWORK
+# ======================================================================================================================
+class Network:
+
+    def __init__(self):
+        self.name = str()
+        self.data_dir = str()
+        self.results_dir = str()
+        self.diagrams_dir = str()
+        self.operational_data_file = str()
+        self.data_loaded = False
+        self.is_transmission = False
+        self.baseMVA = 100.0
+        self.nodes = list()
+        self.loads = list()
+        self.branches = list()
+        self.generators = list()
+        self.energy_storages = list()
+        self.shared_energy_storages = list()
+        self.prob_operation_scenarios = list()          # Probability of operation (generation and consumption) scenarios
+        self.cost_energy_p = list()
+        self.cost_flex = list()
+        self.active_distribution_network_nodes = list()
