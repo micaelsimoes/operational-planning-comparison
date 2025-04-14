7754877fbe92d27652e86bb8d1215c75bb13fa12
micaelsimoes <micaelsimoes@gmail.com>
2025-04-14 10:00:16 +0100

    Initial commit

---
+++ helper_functions.py
@@ -0,0 +1,26 @@
+import sys
+import json
+from definitions import *
+
+
+def read_json_file(filename):
+    try:
+        input_file = open(filename, 'r')
+        with input_file:
+            file_contents = input_file.read()
+            return file_contents
+    except OSError:
+        print(f'[ERROR] Could not open file {filename}. Exiting...')
+        sys.exit()
+
+
+def convert_json_to_dict(json_string):
+    try:
+        data_dict = json.loads(json_string)
+        return data_dict
+    except json.JSONDecodeError as e:
+        print(f'[ERROR] Could not convert JSON to dict. JSONDecodeError: {e}')
+        exit(ERROR_SPECIFICATION_FILE)
+    except TypeError as e:
+        print(f'[ERROR] Could not convert JSON to dict. TypeError: {e}')
+        exit(ERROR_SPECIFICATION_FILE)
