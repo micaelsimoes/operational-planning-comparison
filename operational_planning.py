a6e2ab5ca8112ab69a2835423c5dbce885fd2493
micaelsimoes <micaelsimoes@gmail.com>
2025-04-14 13:53:59 +0100

    Read functions updated.

--- operational_planning.py
+++ operational_planning.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from network import Network
+from operational_planning_parameters import ADMMParameters
 from helper_functions import *
 
 
@@ -19,12 +20,17 @@ class OperationalPlanning:
         self.cost_flex = 0.00
         self.distribution_networks = dict()
         self.transmission_network = Network()
+        self.params = ADMMParameters()
 
     def read_case_study(self):
         _read_case_study(self)
 
     def read_market_data_from_file(self, filename):
         _read_market_data_from_file(self, filename)
+
+    def read_operational_planning_parameters_from_file(self, params_file):
+        filename = os.path.join(self.data_dir, params_file)
+        self.params.read_parameters_from_file(filename)
 
 
 # ======================================================================================================================
@@ -53,10 +59,10 @@ def _read_case_study(operational_planning):
 
         print('[INFO] Reading DISTRIBUTION NETWORK DATA from file(s)...')
 
-        network_name = distribution_network_data['name']  # Network filename
-        params_filename = distribution_network_data['params_filename']  # Params filename
+        network_name = distribution_network_data['name']                                    # Network filename
+        params_filename = distribution_network_data['params_filename']                      # Params filename
         operational_data_filename = distribution_network_data['operational_data_filename']  # Operational data filename
-        connection_nodeid = distribution_network_data['connection_node_id']  # Connection node ID
+        connection_nodeid = distribution_network_data['connection_node_id']                 # Connection node ID
 
         distribution_network = Network()
         distribution_network.name = network_name
@@ -68,6 +74,34 @@ def _read_case_study(operational_planning):
         distribution_network.cost_flex = operational_planning.cost_flex
         distribution_network.read_network_parameters(params_filename)
         distribution_network.read_network_data(operational_data_filename)
+        distribution_network.tn_connection_nodeid = connection_nodeid
+        operational_planning.distribution_networks[connection_nodeid] = distribution_network
+    operational_planning.active_distribution_network_nodes = [node_id for node_id in operational_planning.distribution_networks]
+
+    # Transmission Network
+    print('[INFO] Reading TRANSMISSION NETWORK DATA from file(s)...')
+    transmission_network = Network()
+    transmission_network.name = operational_planning_data['TransmissionNetwork']['name']
+    transmission_network.is_transmission = True
+    transmission_network.data_dir = operational_planning.data_dir
+    transmission_network.results_dir = operational_planning.results_dir
+    transmission_network.diagrams_dir = operational_planning.diagrams_dir
+    transmission_network.cost_energy_p = operational_planning.cost_energy_p
+    transmission_network.cost_flex = operational_planning.cost_flex
+    params_filename = operational_planning_data['TransmissionNetwork']['params_filename']
+    transmission_network.read_network_parameters(params_filename)
+    operational_data_filename = operational_planning_data['TransmissionNetwork']['operational_data_filename']
+    transmission_network.read_network_data(operational_data_filename)
+    transmission_network.active_distribution_network_nodes = [node_id for node_id in operational_planning.distribution_networks]
+    operational_planning.transmission_network = transmission_network
+
+    # Planning Parameters
+    print(f'[INFO] Reading PLANNING PARAMETERS from file...')
+    params_filename = operational_planning_data['PlanningParameters']['params_filename']
+    operational_planning.read_operational_planning_parameters_from_file(params_filename)
+
+
+
 
 
 # ======================================================================================================================
