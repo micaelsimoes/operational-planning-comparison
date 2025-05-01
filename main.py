6f14afe2f4fae0f2cf10a6c8951304727504d081
micaelsimoes <micaelsimoes@gmail.com>
2025-05-01 16:19:48 +0100

    Tests.

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     operational_planning.read_case_study()
 
     t = 11
-    num_steps = 8
+    num_steps = 16
 
     # transmission_network = operational_planning.transmission_network
     # tn_model = transmission_network.build_model(t=11)
