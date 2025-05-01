6ca13363f5e407639983252e045f45878217c149
micaelsimoes <micaelsimoes@gmail.com>
2025-05-01 16:05:32 +0100

    Tests.

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     # transmission_network.write_optimization_results_to_excel(processed_results, filename='test')
 
     for run in range(10):
-        operational_planning.run_without_coordination(t=t, filename=f'{operational_planning.name}_uncoordinated_t={t}')
-        # operational_planning.run_hierarchical_coordination(t=t, num_steps=num_steps, filename=f'{operational_planning.name}_hierarchical_t={t}', print_pq_map=False)
+        # operational_planning.run_without_coordination(t=t, filename=f'{operational_planning.name}_uncoordinated_t={t}')
+        operational_planning.run_hierarchical_coordination(t=t, num_steps=num_steps, filename=f'{operational_planning.name}_hierarchical_t={t}_num_steps={num_steps}_run={run}', print_pq_map=False)
         # operational_planning.run_distributed_coordination(t=t, filename=f'{operational_planning.name}_distributed_t={t}')
         # operational_planning.run_distributed_coordination(t=t, consider_shared_ess=True, filename=f'{operational_planning.name}_distributed_ESS_t={t}')
