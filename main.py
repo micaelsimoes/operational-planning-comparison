742a818d68ccfaaae2c87f61b163b6e8ac4553f5
micaelsimoes <micaelsimoes@gmail.com>
2025-05-01 16:03:20 +0100

    CS2 uploaded.

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     operational_planning.read_case_study()
 
     t = 11
-    num_steps = 4
+    num_steps = 1
 
     # transmission_network = operational_planning.transmission_network
     # tn_model = transmission_network.build_model(t=11)
@@ -69,8 +69,8 @@ if __name__ == '__main__':
     # processed_results = transmission_network.process_results(tn_model, results)
     # transmission_network.write_optimization_results_to_excel(processed_results, filename='test')
 
-    for _ in range(10):
-        # operational_planning.run_without_coordination(t=t, filename=f'{operational_planning.name}_uncoordinated_t={t}')
-        operational_planning.run_hierarchical_coordination(t=t, num_steps=num_steps, filename=f'{operational_planning.name}_hierarchical_t={t}', print_pq_map=False)
+    for run in range(10):
+        operational_planning.run_without_coordination(t=t, filename=f'{operational_planning.name}_uncoordinated_t={t}')
+        # operational_planning.run_hierarchical_coordination(t=t, num_steps=num_steps, filename=f'{operational_planning.name}_hierarchical_t={t}', print_pq_map=False)
         # operational_planning.run_distributed_coordination(t=t, filename=f'{operational_planning.name}_distributed_t={t}')
         # operational_planning.run_distributed_coordination(t=t, consider_shared_ess=True, filename=f'{operational_planning.name}_distributed_ESS_t={t}')
